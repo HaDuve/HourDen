3fbf44527de682c28eb26b6b695af426179ed5c4
Hannes Duve <hannes.duve@outlook.com>
2026-08-02 13:11:10 +0100

    Disambiguate issued-invoice Download from zip export in live test.

--- apps/web/src/InvoicesPage.integration.test.tsx
+++ apps/web/src/InvoicesPage.integration.test.tsx
@@ -146,7 +146,7 @@ describeWithAuthenticatedWorkspace(
 
       const clickSpy2 = vi.spyOn(HTMLAnchorElement.prototype, "click");
       fireEvent.click(
-        screen.getByRole("button", { name: /download/i }),
+        screen.getByRole("button", { name: /^download$/i }),
       );
 
       await waitFor(() => {
